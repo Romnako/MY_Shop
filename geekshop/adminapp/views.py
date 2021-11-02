7631bf4340ba574e089db8b4ba59701ce660ddb2
Romnako <76477868+Romnako@users.noreply.github.com>
2021-11-02 13:23:14 +0300

    lesson 8 cbv

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -2,42 +2,76 @@ from django.http import HttpResponseRedirect
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render, reverse
 from mainapp.models import Product, ProductCategory
-from  django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test
 from authapp.forms import ShopUserRegisterForm
 from adminapp.forms import ShopUserAdminEditForm, ProductEditForm
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def users(request):
-    title = 'админка/пользователи'
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = "adminapp/users.html"
+    context_object_name = 'object'
 
-    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UsersListView, self).get_context_data()
+        context['title'] = 'админка/пользователи'
+        context.update({'title': 'админка/пользователи'})
 
-    context = {
-        'title': title,
-        'objects': users_list
-    }
+        return context
 
-    return render(request, 'adminapp/users.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs ):
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        get_con = seper(UsersListView, self).get()
+        return get_con
+
+    def get_queryset(self):
+        return ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def users(request):
+#     title = 'админка/пользователи'
+#
+#     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+#
+#     context = {
+#         'title': title,
+#         'objects': users_list
+#     }
+#
+#     return render(request, 'adminapp/users.html', context)
 
 
-def user_create(request):
+# def user_create(request):
+#
+#     title = 'пользователи/создание'
+#
+#     if request.method == 'POST':
+#         user_form = ShopUserRegisterForm(request.POST, request.FILES)
+#         if user_form.is_valid():
+#             user_form.save()
+#             return HttpResponseRedirect(reverse('admin_staff:users'))
+#         else:
+#             user_form = ShopUserRegisterForm()
+#     context = {
+#         'title': title,
+#         'user_form': user_form
+#     }
+#
+#     return render(request, 'adminapp/user_update.html', context)
 
-    title = 'пользователи/создание'
+class UserCreateView(CreateView):
+    model = ShopUser
+    form_class = ShopUserRegisterForm
+    template_name = 'adminapp/user_update.html'
+    success_url = reverse_lazy('admin_staff:users')
 
-    if request.method == 'POST':
-        user_form = ShopUserRegisterForm(request.POST, request.FILES)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse('admin_staff:users'))
-        else:
-            user_form = ShopUserRegisterForm()
-    context = {
-        'title': title,
-        'user_form': user_form
-    }
 
-    return render(request, 'adminapp/user_update.html', context)
 
 
 def user_update(request, pk):
